0704722dd3cd03b76493b841d3c099ecaa88d21f
Justin Garber <JustinGarber@outlook.com>
2023-05-17 06:22:20 -0700

    change

--- models.py
+++ models.py
@@ -53,7 +53,7 @@ class OrderReceived(db.Model):
 
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
