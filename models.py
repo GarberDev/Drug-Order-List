019747ef046dbcbb067cacc58244b60fbae23e58
Justin Garber <JustinGarber@outlook.com>
2023-05-17 07:22:36 -0700

    users=user

--- models.py
+++ models.py
@@ -19,7 +19,7 @@ def connect_db(app):
 
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
