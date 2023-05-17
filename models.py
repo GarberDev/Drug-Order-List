11abcf42b455eecaf2b0ef07bc95dbcf3e845dcd
Justin Garber <JustinGarber@outlook.com>
2023-05-17 05:44:58 -0700

    db change

--- models.py
+++ models.py
@@ -1,10 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 db = SQLAlchemy()
 
 
 def connect_db(app):
+    # Retrieve the DB URL from environment variables
+    db_url = os.environ.get(
+        'postgres://drug_list_user:6OloIXwOMDOgHVmjWwQatrARDlGnwxwn@dpg-chi7iql269vf5qb7gsn0-a/drug_list')
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
     db.app = app
     db.init_app(app)
 
