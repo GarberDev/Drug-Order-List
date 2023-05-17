f24bd71ceec0ec21187cba59a87be4674ae12f16
Justin Garber <JustinGarber@outlook.com>
2023-05-17 05:54:50 -0700

    change

--- models.py
+++ models.py
@@ -9,7 +9,7 @@ def connect_db(app):
     # Retrieve the DB URL from environment variables
     db_url = os.environ.get(
         'postgres://drug_list_user:6OloIXwOMDOgHVmjWwQatrARDlGnwxwn@dpg-chi7iql269vf5qb7gsn0-a/drug_list')
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+    app.config['drug_list'] = db_url
     db.app = app
     db.init_app(app)
 
